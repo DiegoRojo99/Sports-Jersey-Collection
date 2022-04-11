3e5468c2d2f094e7a8c18f0027f3a7c116080398
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-11 19:56:46 +0200

    Created a NBA page

--- app/src/main/java/com/youngdred/sports_jersey_collection/LoginActivity.java
+++ app/src/main/java/com/youngdred/sports_jersey_collection/LoginActivity.java
@@ -2,7 +2,9 @@ package com.youngdred.sports_jersey_collection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nbaButton=(Button) findViewById(R.id.home_nba_btn);
+        nbaButton.setOnClickListener(view -> goToNBA());
+    }
+
+    void goToNBA(){
+        Intent NBAintent=new Intent(LoginActivity.this, NBA_Activity.class);
+        startActivity(NBAintent);
     }
 }
