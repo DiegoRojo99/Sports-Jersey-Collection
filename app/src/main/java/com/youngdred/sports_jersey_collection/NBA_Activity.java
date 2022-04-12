3d2255289c97aff86dce5d034a791c366c3e2004
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-12 12:31:01 +0200

    All east conference teams in NBA with name Most with image

--- app/src/main/java/com/youngdred/sports_jersey_collection/NBA_Activity.java
+++ app/src/main/java/com/youngdred/sports_jersey_collection/NBA_Activity.java
@@ -13,11 +13,16 @@ public class NBA_Activity extends AppCompatActivity {
     TextView atlTV, bosTV, bknTV, chaTV, chiTV, cleTV, detTV, indTV, miaTV, milTV, nykTV, orlTV, phiTV, torTV, wasTV;
     View atlV, bosV, bknV, chaV, chiV, cleV, detV, indV, miaV, milV, nykV, orlV, phiV, torV, wasV;
 
+    ImageView nbaIV, eastIV, westIV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nba);
+
+        nbaIV=findViewById(R.id.iv_nba_logo);
+        nbaIV.setImageResource(R.drawable.nba_logo);
 
         atlV=findViewById(R.id.include_atlanta_hawks_team);
         atlTV=atlV.findViewById(R.id.tv_team_name);
@@ -41,18 +46,72 @@ public class NBA_Activity extends AppCompatActivity {
         chaTV=chaV.findViewById(R.id.tv_team_name);
         chaTV.setText(R.string.cha);
         chaIV=chaV.findViewById(R.id.iv_team_image);
-        //chaIV.setImageResource(R.drawable.charlotte_hornets_logo);
+        chaIV.setImageResource(R.drawable.charlotte_hornets_logo);
 
         chiV=findViewById(R.id.include_chicago_bulls_team);
         chiTV=chiV.findViewById(R.id.tv_team_name);
         chiTV.setText(R.string.chi);
         chiIV=chiV.findViewById(R.id.iv_team_image);
-        //chaIV.setImageResource(R.drawable.charlotte_hornets_logo);
+        chiIV.setImageResource(R.drawable.chicago_bulls_logo);
+
+        cleV=findViewById(R.id.include_cleveland_cavaliers_team);
+        cleTV=cleV.findViewById(R.id.tv_team_name);
+        cleTV.setText(R.string.cle);
+        cleIV=cleV.findViewById(R.id.iv_team_image);
+        cleIV.setImageResource(R.drawable.cleveland_cavaliers_logo);
+
+        detV=findViewById(R.id.include_detroit_pistons_team);
+        detTV=detV.findViewById(R.id.tv_team_name);
+        detTV.setText(R.string.cle);
+        detIV=detV.findViewById(R.id.iv_team_image);
+        detIV.setImageResource(R.drawable.detroit_pistons_logo);
+
+        indV=findViewById(R.id.include_indiana_pacers_team);
+        indTV=indV.findViewById(R.id.tv_team_name);
+        indTV.setText(R.string.ind);
+        indIV=indV.findViewById(R.id.iv_team_image);
+        indIV.setImageResource(R.drawable.indiana_pacers_logo);
+
+        miaV=findViewById(R.id.include_miami_heat_team);
+        miaTV=miaV.findViewById(R.id.tv_team_name);
+        miaTV.setText(R.string.mia);
+        miaIV=miaV.findViewById(R.id.iv_team_image);
+        miaIV.setImageResource(R.drawable.miami_heat_logo);
+
+        milV=findViewById(R.id.include_milwaukee_bucks_team);
+        milTV=milV.findViewById(R.id.tv_team_name);
+        milTV.setText(R.string.mil);
+        milIV=milV.findViewById(R.id.iv_team_image);
+        milIV.setImageResource(R.drawable.milwaukee_bucks_logo);
+
+        nykV=findViewById(R.id.include_new_york_knicks_team);
+        nykTV=nykV.findViewById(R.id.tv_team_name);
+        nykTV.setText(R.string.nyk);
+        nykIV=nykV.findViewById(R.id.iv_team_image);
+        //cleIV.setImageResource(R.drawable.cleveland_cavaliers_logo);
+
+        orlV=findViewById(R.id.include_orlando_magic_team);
+        orlTV=orlV.findViewById(R.id.tv_team_name);
+        orlTV.setText(R.string.orl);
+        orlIV=orlV.findViewById(R.id.iv_team_image);
+        orlIV.setImageResource(R.drawable.orlando_magic_logo);
+
+        phiV=findViewById(R.id.include_philadelphia_76ers_team);
+        phiTV=phiV.findViewById(R.id.tv_team_name);
+        phiTV.setText(R.string.phi);
+        phiIV=phiV.findViewById(R.id.iv_team_image);
+        //cleIV.setImageResource(R.drawable.cleveland_cavaliers_logo);
 
         torV=findViewById(R.id.include_toronto_raptors_team);
         torTV=torV.findViewById(R.id.tv_team_name);
         torTV.setText(R.string.tor);
         torIV=torV.findViewById(R.id.iv_team_image);
         torIV.setImageResource(R.drawable.toronto_raptors_logo);
+
+        wasV=findViewById(R.id.include_washington_wizards_team);
+        wasTV=wasV.findViewById(R.id.tv_team_name);
+        wasTV.setText(R.string.was);
+        wasIV=wasV.findViewById(R.id.iv_team_image);
+        wasIV.setImageResource(R.drawable.washington_wizards_logo);
     }
 }
